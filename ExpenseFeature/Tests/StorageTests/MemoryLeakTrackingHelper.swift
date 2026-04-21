da929962b6f79d040a1ba4e1abb444b903cfef80
Hiren Fadadu <hirenfaldu9697@gmail.com>
2026-04-21 05:51:48 +0530

    Add scheme for storage tests Add memory leak tracking helper function

---
+++ ExpenseFeature/Tests/StorageTests/MemoryLeakTrackingHelper.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  ExpenseFeature
+//
+//  Created by Hirenkumar Fadadu on 20/04/26.
+//
+
+import Testing
+
+private final class WeakReference {
+    weak var object: AnyObject?
+    init(_ object: AnyObject?) { self.object = object }
+}
+
+func withMemoryLeakTracking(sourceLocation: SourceLocation = #_sourceLocation,
+                            testBody: (_ trackForMemoryLeaks: (AnyObject?...) -> Void) -> Void) {
+    var weakReferences: [WeakReference] = []
+    let track: (AnyObject?...) -> Void = { instances in
+        weakReferences.append(contentsOf: instances.map(WeakReference.init))
+    }
+    
+    testBody(track)
+    
+    for weakReference in weakReferences {
+        #expect(weakReference.object == nil, "Potential memory leak.", sourceLocation: sourceLocation)
+    }
+}
+
--- ExpenseFeature/Tests/StorageTests/StorageTests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  ExpenseFeature
-//
-//  Created by Hirenkumar Fadadu on 19/04/26.
-//
-
-import Foundation
