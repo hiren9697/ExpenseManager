1b25a6322fb7043bb463e584b5938f2d29ad9c32
Hiren Fadadu <hirenfaldu9697@gmail.com>
2026-05-17 19:15:11 +0530

    Add empty tests for save expense view controller tests

--- ExpenseUI_UIKit/Tests/SaveExpenseViewControllerTest/SaveExpenseViewControllerTests.swift
+++ ExpenseUI_UIKit/Tests/SaveExpenseViewControllerTest/SaveExpenseViewControllerTests.swift
@@ -8,6 +8,38 @@
 import Testing
 
 struct SaveExpenseViewControllerTests {
-    @Test func test() async throws {
+    @Test
+    func initialInputFieldsValues() async throws {
+        
+    }
+    
+    @Test
+    func dateInputField_hasDatePickerInputView_withCorrectDateSelection() async throws {
+        
+    }
+    
+    @Test
+    func inputFields_hasCorrectSubmitButtons() async throws {
+        
+    }
+    
+    @Test
+    func saveButton_updatesState_basedOnInputs() async throws {
+        
+    }
+    
+    @Test 
+    func doesNotSendSaveRequest_onInit() async throws {
+        
+    }
+    
+    @Test
+    func presentsErrorAlert_onInvalidInputs() async throws {
+        
+    }
+    
+    @Test
+    func sendsSaveRequest_onValidInputs() async throws {
+        
     }
 }
